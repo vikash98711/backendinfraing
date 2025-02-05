5a824a646b97cacdb764c4070796bdbbebaf63af
vikash98711 <vikashrstylo123456@gmail.com>
2025-02-05 16:10:06 +0530

    update

--- New folder/config/multerConfig.js
+++
@@ -1,7 +0,0 @@
-// multerConfig.js
-import multer from 'multer';
-
-// You don't need to define storage since you're uploading directly to Cloudinary
-const upload = multer();
-
-export default upload;
